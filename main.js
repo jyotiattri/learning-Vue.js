ba70cf03fb6531887b7158cb428dd52bf158652b
jyoti attri <jyotiatri22@gmail.com>
2020-04-14 15:11:34 +0530

    computed properties

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
 var app = new Vue({
     el: '#app',
     data: {
+      brand: 'Vue Mastery',
       product: 'Socks',
-      image: './image/vmSocks-green-onWhite.jpg',
       altText: 'A pair of Socks',
+      selectedVariant: 0,
       link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
-      inStock: false,
-      details: ["80% Ployster", "20% Cotton", "Gender Neutral"],
+     details: ["80% Ployster", "20% Cotton", "Gender Neutral"],
       varients:[
         {
           varientId: 2234,
           varientColor: 'green',
-          varientImage: "./image/vmSocks-green-onWhite.jpg"
+          varientImage: "./image/vmSocks-green-onWhite.jpg",
+          varientQuantity: 10
         },
         {
           varientId: 2235,
           varientColor: 'blue',
-          varientImage: "./image/vmSocks-blue-onWhite.jpg"
+          varientImage: "./image/vmSocks-blue-onWhite.jpg",
+          varientQuantity: 0
         }
 
       ],
         cart: 0,
-        num: 1011
-
+        num: 1011,
     },  
       methods: {
-        addToCart() {
+        addToCart:function() {
           this.cart +=1
         },
-      deleteFromCart() {
+      deleteFromCart: function() {
         this.cart -=1
       },
-      updateProduct(varientImage) {
-        this.image = varientImage
+      updateProduct: function(index) {
+        this.selectedVariant = index
+        console.log(index)
       }
-    }
-
+      },
+      computed:{
+        title() {
+          return this.brand + ' ' + this.product
+        },
+    image(){
+      return this.varients[this.selectedVariant].varientImage
+    }, 
+    inStock(){
+      return this.varients[this.selectedVariant].varientQuantity
+    }, 
+  }
     }
   )
